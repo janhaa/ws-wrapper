b6bc1f55974c43b552c44f0eccff9ce890dae64e
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-04-13 12:52:56 +0200

    Initial version.

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import WebSocket from 'ws';
+import EventEmitter from 'events';
+
+class WebSocketWrapper extends EventEmitter {
+  url: string;
+  connection: WebSocket | null = null;
+
+  constructor(url: string) {
+    super();
+    this.url = url;
+  }
+
+  connect() {
+    if (this.connection === null) {
+      this.connection = new WebSocket(this.url);
+    }
+
+    this.connection.onopen = function () { };
+
+    this.connection.onmessage = function (event) {
+      this.emit('message', event.data);
+    };
+
+    this.connection.onclose = function () {
+      this.connection = undefined;
+      setTimeout(() => this.connect(), 1000);
+    };
+
+    this.connection.onerror = function () {
+      this.connection = undefined;
+      setTimeout(() => this.connect(), 1000);
+    };
+  }
+
+  close() {
+    if (this.connection != null || this.connection != undefined) {
+      this.connection.close();
+      this.connection = undefined;
+    }
+  }
+}
+
+export { WebSocketWrapper };
