ef24a91a37480c0af806806e09e3901270d8b4a3
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-08-02 21:02:19 +0200

    Cleanly implement reconnect and event re-emitting.

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,23 @@ class WebSocketWrapper extends EventEmitter {
   url: string;
   connection: WebSocket | null = null;
 
+  #allEvents: Array<string> = [
+    "close",
+    "error",
+    "upgrade",
+    "message",
+    "open",
+    "ping",
+    "pong",
+    "unexpected-response",
+  ];
+
   constructor(url: string) {
     super();
     this.url = url;
   }
 
-  isConnected(connection: WebSocket | null) : connection is WebSocket {
+  isConnected(connection: WebSocket | null): connection is WebSocket {
     if (!this.connection) return false;
     return this.connection.readyState === WebSocket.OPEN;
   }
@@ -25,21 +36,25 @@ class WebSocketWrapper extends EventEmitter {
       this.connection = new WebSocket(this.url);
     }
 
-    this.connection.onopen = () => {};
-
-    this.connection.onmessage = (event) => {
-      this.emit("message", event.data);
-    };
-
-    this.connection.onclose = () => {
+    const initiateReconnect = () => {
       this.connection = null;
       setTimeout(() => this.connect(), 1000);
     };
 
-    this.connection.onerror = () => {
-      this.connection = null;
-      setTimeout(() => this.connect(), 1000);
+    const interceptors: { [key: string]: Function } = {
+      close: initiateReconnect,
     };
+
+    // catch all specified events, optionally intercept and forward
+    this.#allEvents.forEach((event) => {
+      (this.connection as WebSocket).on(event, (...args) => {
+        // intercept event
+        if (event in interceptors) interceptors[event]();
+
+        // forward event
+        this.emit(event, ...args);
+      });
+    });
   }
 
   close() {
