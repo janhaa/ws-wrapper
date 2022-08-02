d46661c074d6e22eed5f2b29a2c5a778202864da
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2022-08-02 20:48:13 +0200

    Implement sending data.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import WebSocket from 'ws';
-import EventEmitter from 'events';
+import WebSocket from "ws";
+import EventEmitter from "events";
 
 class WebSocketWrapper extends EventEmitter {
   url: string;
@@ -10,15 +10,25 @@ class WebSocketWrapper extends EventEmitter {
     this.url = url;
   }
 
+  isConnected(connection: WebSocket | null) : connection is WebSocket {
+    if (!this.connection) return false;
+    return this.connection.readyState === WebSocket.OPEN;
+  }
+
+  send(data: any) {
+    if (!this.isConnected(this.connection)) throw new Error("not connected");
+    this.connection.send(data);
+  }
+
   connect() {
     if (this.connection === null) {
       this.connection = new WebSocket(this.url);
     }
 
-    this.connection.onopen = () => { };
+    this.connection.onopen = () => {};
 
     this.connection.onmessage = (event) => {
-      this.emit('message', event.data);
+      this.emit("message", event.data);
     };
 
     this.connection.onclose = () => {
